821e4bb572fd348555e3a94f02abeae923c5192e
Hack2Health <hemashree21.msh@gmail.com>
2024-05-22 19:47:14 +0530

    Updated Contact Us Page
    Signed-off-by: Hemashree M S <hemashree21.msh@gmail.com>

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import Layout from "@theme/Layout";
 import styles from './styles.module.css';
 
+/**
+ * Contact Us Page
+ */
+
 const Index = () => {
   return (
     <Layout>
       <h1 style={{marginLeft: '2rem', marginTop: '1rem'}}>Contact Us</h1>
       <div className={styles.division}>
       <form className={styles.myform}>
-        <label className={styles.mylabel}>Name</label>
+        <label className={styles.mylabel} htmlFor="name">Name</label>
         <input placeholder='Enter your name' className={styles.myinput} type="text" name="user_name" />
-        <label className={styles.mylabel}>Email</label>
+        <label className={styles.mylabel} htmlFor="email">Email</label>
         <input placeholder='Enter your email' className={styles.myinput} type="email" name="user_email" />
-        <label className={styles.mylabel}>Message</label>
+        <label className={styles.mylabel} htmlFor="message">Message</label>
         <textarea placeholder='Your message...' className={styles.mytextarea} name="message" />
         <button className={styles.inputsubmit} type="submit">Send</button>
       </form>
