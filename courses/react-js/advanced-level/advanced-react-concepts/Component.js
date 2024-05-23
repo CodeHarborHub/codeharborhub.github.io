8d6b67f71de384f918b42d792a8562f0cca0dfb2
AMISH-KHAN <khanamish8923@gmail.com>
2024-05-24 00:30:33 +0530

    closes issue #165

--- courses/react-js/advanced-level/advanced-react-concepts/Component.js
+++ courses/react-js/advanced-level/advanced-react-concepts/Component.js
@@ -1,7 +1,12 @@
 import withLogger from "./withLogger";
+import PropTypes from 'prop-types';
 
 const Component = ({ message }) => {
   return <div>{message}</div>;
+};
+
+Component.propTypes = {
+  message: PropTypes.string.isRequired
 };
 
 export default withLogger(Component);
