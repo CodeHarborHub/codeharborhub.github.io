3c623858f9c0f1442336d21ec1dc43d392a4fe5e
AryanAg08 <aryangoyal90@outlook.com>
2024-05-31 09:26:07 +0530

    Modifying Counter.js for deepscan Check fail

--- courses/react-js/begginer-level/building-your-first-react-app/Counter.js
+++ courses/react-js/begginer-level/building-your-first-react-app/Counter.js
@@ -7,7 +7,9 @@ class Counter extends Component {
   }
 
   incrementCount = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
   };
 
   render() {
