6d8f7b50bb6744576508939c14b457b7fe8ce6cf
kdharsh24 <harshkumardas24@gmail.com>
2024-06-03 19:39:58 +0530

    Doing it properly of problem 150

--- src/components/HomePage/Courses/index.tsx
+++ src/components/HomePage/Courses/index.tsx
@@ -20,42 +20,41 @@ const Courses: React.FC<CoursesProps> = ({ courses }) => {
     const boxHeight=allBox[0].offsetHeight;
     const div=document.createElement("div");
     div.style.position="absolute";
-	  div.style.height=boxHeight + "px";
-	  div.style.width=boxWidth + "px";
-	  div.style.background="linear-gradient( 30deg, rgba(206, 86, 174, 0.972) 0%, rgb(253 29 29 / 100%) 50%, rgb(252 176 69 / 100%) 100%)";
+	  div.style.height=`${boxHeight}px`;
+	  div.style.width=`${boxWidth}px`;
+	  div.style.background="linear-gradient(30deg, rgba(206, 86, 174, 0.972) 0%, rgb(253 29 29 / 100%) 50%, rgb(252 176 69 / 100%) 100%)";
 	  div.style.transition="all 1s ease";
 	  div.style.zIndex="1";
-    div.style.left=allBox[0].offsetLeft + "px";
-    div.style.top=allBox[0].offsetTop + "px";
+    div.style.left=`${allBox[0].offsetLeft}px`;
+    div.style.top=`${allBox[0].offsetTop}px`;
     div.style.display = "none"
     const bigbox = document.querySelector("main");
     bigbox.appendChild(div)
-    for(let i=0; i<allBox.length; i++){
-      allBox[i].addEventListener("mouseover", function(){
+    for(let boxelement of allBox){
+      boxelement.addEventListener("mouseover", function(){
       const height=this.offsetHeight;
       const width=this.offsetWidth;
       const x=this.offsetLeft;           	 	
       const y=this.offsetTop;
       div.style.display = "block";
-      div.style.left=x + "px";
-      div.style.top=y + "px";
-      div.style.width=width + "px";
-      div.style.height=height + "px";
+      div.style.left= `${x}px`;
+      div.style.top= `${y}px`;
+      div.style.width =`${width}px`;
+      div.style.height= `${height}px`;
     })
     const allfeatureBox=document.querySelectorAll<HTMLElement>(".feature_item_src-components-HomePage-home-module");
-    const boxWidth=allfeatureBox[0].offsetWidth;
-    const boxHeight=allfeatureBox[0].offsetHeight;
-    for(let i=0; i<allfeatureBox.length; i++){
-      allfeatureBox[i].addEventListener("mouseover", function(){
+
+    for(let boxelement of allfeatureBox){
+      boxelement.addEventListener("mouseover", function(){
       const height=this.offsetHeight;
       const width=this.offsetWidth;
       const x=this.offsetLeft;           	 	
       const y=this.offsetTop;
       div.style.display = "block";
-      div.style.left=x + "px";
-      div.style.top=y + "px";
-      div.style.width=width + "px";
-      div.style.height=height + "px";
+      div.style.left= `${x}px`;
+      div.style.top= `${y}px`;
+      div.style.width =`${width}px`;
+      div.style.height= `${height}px`;
     })
     const mainitem = document.getElementsByTagName("main")
     mainitem[0].addEventListener("mouseleave", function(){
