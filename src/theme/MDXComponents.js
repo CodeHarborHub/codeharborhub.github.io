570481b89b085f198e41c8e4a8c4942f7a74d7a4
Restyled.io <commits@restyled.io>
2024-06-29 11:44:44 +0000

    Restyled by clang-format

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,28 +1,34 @@
-import MDXComponents from '@theme-original/MDXComponents';
-import Tabs from '@theme/Tabs';
-import TabItem from '@theme/TabItem';
-import SolutionAuthor from '@site/src/components/SolutionAuthor';
-import TutorialAuthors from '@site/src/components/TutorialAuthors';
-import Contributors from '@site/src/components/Contributors';
-import Table from '@site/src/components/Table';
-import ArrayVisualizations from '@site/src/components/DSA/arrays/ArrayVisualizations';
-import SelectionSortVisualization from '@site/src/components/DSA/arrays/SelectionSortVisualization';
-import BubbleSortVisualization from '@site/src/components/DSA/arrays/BubbleSortVisualization';
-import BrowserWindow from '@site/src/components/BrowserWindow';
-import GiscusComponent from '@site/src/components/GiscusComponent';
-import CollectionList from '@site/src/components/CollectionList';
-import { FaReact } from 'react-icons/fa';
-import Courses from '@site/src/components/Courses';
-import DocCardList from '@theme/DocCardList';
-import Highlight from '@site/src/components/Highlight';
-import InsertionSortVisualization from '@site/src/components/DSA/arrays/InsertionSortVisualization';
-import File from '@site/src/components/File';
-import Lesson from '@site/src/components/Lesson';
-import Image from '@theme/IdealImage';
-import LiteYouTubeEmbed from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
+
 import Author from '@site/src/components/Author';
-import LinearSearchVisualizer from '@site/src/dsa/searching-algorithms/LinearSearchVisualizer';
+import BrowserWindow from '@site/src/components/BrowserWindow';
+import CollectionList from '@site/src/components/CollectionList';
+import Contributors from '@site/src/components/Contributors';
+import Courses from '@site/src/components/Courses';
+import ArrayVisualizations from
+    '@site/src/components/DSA/arrays/ArrayVisualizations';
+import BubbleSortVisualization from
+    '@site/src/components/DSA/arrays/BubbleSortVisualization';
+import InsertionSortVisualization from
+    '@site/src/components/DSA/arrays/InsertionSortVisualization';
+import SelectionSortVisualization from
+    '@site/src/components/DSA/arrays/SelectionSortVisualization';
+import File from '@site/src/components/File';
+import GiscusComponent from '@site/src/components/GiscusComponent';
+import Highlight from '@site/src/components/Highlight';
+import Lesson from '@site/src/components/Lesson';
+import SolutionAuthor from '@site/src/components/SolutionAuthor';
+import Table from '@site/src/components/Table';
+import TutorialAuthors from '@site/src/components/TutorialAuthors';
+import LinearSearchVisualizer from
+    '@site/src/dsa/searching-algorithms/LinearSearchVisualizer';
+import MDXComponents from '@theme-original/MDXComponents';
+import DocCardList from '@theme/DocCardList';
+import Image from '@theme/IdealImage';
+import TabItem from '@theme/TabItem';
+import Tabs from '@theme/Tabs';
+import {FaReact} from 'react-icons/fa';
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
 
 export default {
   // Re-use the default mapping
