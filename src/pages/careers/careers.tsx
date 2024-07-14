495fb904e011c061974645fa1ea547ba119ce482
sivaprasath2004 <prasathsiva2004@gmail.com>
2024-07-14 09:18:53 +0530

    finish

---
+++ src/pages/careers/careers.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from "@theme/Layout";
+
+
+const careers = () => {
+  return (
+    <Layout>
+    <div>careers</div>
+    </Layout>
+  )
+}
+
+export default careers
