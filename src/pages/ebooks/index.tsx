7b748ebb48c3ddcff3895917fc6698697164e340
ParnaRoyChowdhury777 <parnaroychowdhury2020@gmail.com>
2024-07-19 15:25:16 -0700

    Issue#2641 fixed.

--- src/pages/ebooks/index.tsx
+++ src/pages/ebooks/index.tsx
@@ -5,7 +5,7 @@ import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import books from "../../database/ebooks";
 
-export default function Blogs() {
+export default function Books() {
   const { siteConfig } = useDocusaurusContext();
 
   return (
