cac4eb8baa5a2dda29cc88ec3abb3501b991e0d1
Komal Agarwal <komal.agarwal5115@gmail.com>
2024-07-21 13:51:30 +0530

    blogs pulled from main

--- src/database/blogs/index.tsx
+++ src/database/blogs/index.tsx
@@ -1,85 +1,60 @@
 interface Blog {
-    id: number;
-    title: string;
-    image: string;
-    description: string;
-    link: string;
-  }
+  id: number;
+  title: string;
+  image: string;
+  description: string;
+  slug: string;
+}
 
-  const blogs: Blog[] = [
-    {
-      id: 1,
-      title: "Building RESTful CRUD API in springboot",
-      image: "img/blogs/image01.png",
-      description:
-        " RESTful APIs have become a cornerstone for building scalable, efficient, and maintainable web applications.",
-      link: "Building RESTful CRUD API in springboot",
-    },
-    {
-      id: 2,
-      title: "Getting started with Microservices",
-      image: "/assets/images/image01-736d71e3db56657987594b3b11459b5d.png",
-      description:
-        "Microservices are an architectural style that structures an application as a collection of small, loosely coupled services.",
-      link: "getting-started-with-microservices",
-    },
-    {
-      id: 3,
-      title: "Cryptography and Its Use in Cyber Security",
-      image: "/img/blogs/introduction-to-cryptography-and-cyber-security.jpg",
-      description:
-        "In the realm of cyber security, cryptography stands as a critical tool for protecting information.",
-      link: "introduction-to-cryptography-and-cyber-security",
-    },
-    {
-      id: 4,
-      title: "Getting started with Mern",
-      image: "/img/blogs/mern.jpg",
-      description:
-        " Learn the basics of the MERN stack, from building an API with Express.js to creating a React app.",
-      link: "getting-started-with-mern",
-    },
-    {
-      id: 5,
-      title: "Getting started with Vite",
-      image: "/img/blogs/react-and-vite.jpg",
-      description:
-        "Learn how to get started with React by creating a new app using Vite. Follow the steps to set up your development environment",
-      link: "getting-started-with-vite",
-    },
-    {
-      id: 6,
-      title: "VS Code: Fix Bug in Any Extension by Rebuilding It",
-      image: "/img/blogs/build-done-vsce.png",
-      description:
-        "How to fix a bug in any VS Code extension (.vsix) by rebuilding that extension",
-      link: "fix-bug-in-vscode-extension",
-    },
-    {
-      id: 7,
-      title: "Dockerizing a Rust application with Multi-Stage Builds",
-      image: "/img/blogs/rust-docker.png",
-      description:
-        "Learn the basics of the MERN stack, from building an API with Express.js to creating a React app.",
-      link: "dockerizing-a-rust-application-with-multi-stage-builds",
-    },
-    {
-      id: 8,
-      title: "Git Best Practices: Commit Often, Perfect Later, Publish Once",
-      image: "/img/svg/coding.svg",
-      description:
-        "Git is a powerful tool for managing the development of software projects, but it can be challenging to use effectively. ",
-      link: "git-best-practicies.md",
-    },
-    {
-      id: 9,
-      title: "Mastering Data Structures in Python",
-      image: "/img/svg/programming.svg",
-      description:
-        "Data structures are essential components in computer science, enabling efficient data storage, manipulation, and retrieval.",
-      link: "Mastering Data Structures in Python.md",
-    },
-  ];
-  
+const blogs: Blog[] = [
+  {
+    id: 1,
+    title: "Getting started with Microservices",
+    image: "/assets/images/image01-736d71e3db56657987594b3b11459b5d.png",
+    description:
+      "Microservices are an architectural style that structures an application as a collection of small, loosely coupled services.",
+    slug: "getting-started-with-microservices",
+  },
+  {
+    id: 2,
+    title: "Cryptography and Its Use in Cyber Security",
+    image: "/img/blogs/introduction-to-cryptography-and-cyber-security.jpg",
+    description:
+      "In the realm of cyber security, cryptography stands as a critical tool for protecting information.",
+    slug: "introduction-to-cryptography-and-cyber-security",
+  },
+  {
+    id: 3,
+    title: "Blog 3 Title",
+    image: "/img/svg/blogging.svg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.",
+    slug: "#",
+  },
+  {
+    id: 4,
+    title: "Blog 4 Title",
+    image: "/img/svg/blogging.svg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.",
+    slug: "#",
+  },
+  {
+    id: 5,
+    title: "Blog 5 Title",
+    image: "/img/svg/blogging.svg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.",
+    slug: "#",
+  },
+  {
+    id: 6,
+    title: "Blog 6 Title",
+    image: "/img/svg/blogging.svg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.",
+    slug: "#",
+  },
+];
 
-  export default blogs;
+export default blogs;
--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -1,117 +1,61 @@
-import React, { useState, useMemo, useEffect } from "react";
+import React from "react";
+import "./style.css";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Head from "@docusaurus/Head";
-import BlogPage from "../../components/Blogs/BlogPage";
 import Layout from "@theme/Layout";
-import Translate, { translate } from "@docusaurus/Translate";
-import { motion } from "framer-motion";
-import styles from "./styles.module.css";
-
-const TITLE = "CodeHarborHub Blogs";
-const DESCRIPTION =
-  "Discover insights, tips, and trends on Code Harbor Hub's Blog - where innovation meets expertise";
-const EDIT_URL = "https://github.com/codeharborhub/codeharborhub";
+import Link from "@docusaurus/Link";
+import blogs from "../../database/blogs";
+import Head from "@docusaurus/Head";
 
 interface Blog {
   id: number;
   title: string;
   image: string;
   description: string;
-  link: string;
+  slug: string;
 }
 
 export default function Blogs() {
   const { siteConfig } = useDocusaurusContext();
 
-type UserState = {
-  scrollTopPosition: number;
-  focusedElementId: string | undefined;
-};
-
-
-function BlogsHeader() {
   return (
-    <section className="margin-top--lg margin-bottom--lg text--center">
-      <motion.h1
-        initial={{ opacity: 0, x: -15 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        viewport={{ once: true }}
-        transition={{
-          duration: 1,
-          type: "spring",
-          stiffness: 100,
-          delay: 0.2,
-        }}
-        className={styles.TitleText}
-      >
-        {TITLE}
-      </motion.h1>
-      <motion.p
-        initial={{ opacity: 0, y: 15 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        transition={{
-          duration: 1,
-          type: "spring",
-          stiffness: 100,
-          delay: 0.3,
-        }}
-      >
-        {DESCRIPTION}
-      </motion.p>
-
-      <motion.div
-        initial={{ opacity: 0, y: 15 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        transition={{
-          duration: 1,
-          type: "spring",
-          stiffness: 100,
-          delay: 0.5,
-        }}
-      >
-        <a
-          className="button button--primary"
-          href={EDIT_URL}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Translate id="showcase.header.button">
-            🌟 Join the CodeHarbarHub Community
-          </Translate>
-        </a>
-      </motion.div>
-    </section>
-  );
-}
-
-
-  return (
-    <main>
-      <Layout
-        title={`Blogs from ${siteConfig.title}`}
-        description="Discover insights, tips, and trends on Code Harbor Hub's Blog - where innovation meets expertise"
-      >
-        <Head>
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5832817025080991"
-            crossOrigin="anonymous"
-          />
-          <script
-            async
-            custom-element="amp-auto-ads"
-            src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"
-          />
-          <meta
-            name="google-adsense-account"
-            content="ca-pub-5832817025080991"
-          />
-        </Head>
-        <BlogsHeader />
-        <BlogPage />
-      </Layout>
-    </main>
+    <Layout
+      title={`Blogs from ${siteConfig.title}`}
+      description="Welcome to CodeHarborHub Bloge. Learn the basics to advanced concepts of web development. html, css, javascript, react, node.js, dsa, and more."
+    >
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5832817025080991"
+          crossOrigin="anonymous"
+        />
+        <script
+          async
+          custom-element="amp-auto-ads"
+          src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"
+        />
+        <meta name="google-adsense-account" content="ca-pub-5832817025080991" />
+      </Head>
+      <section className="blog-page margin-top--lg margin-bottom--lg">
+        <div className="blog-header">
+          <h1 className="TitleText">Welcome to CodeHarborHub Blogs </h1>
+          <p className="SubTitleText">
+            Learn the basics to advanced concepts of web development. html, css,
+            javascript, react, node.js, dsa, and more.
+          </p>
+        </div>
+        <div className="card-container">
+          {blogs.map((blog) => (
+            <div key={blog.id} className="card">
+              <img src={blog.image} alt={blog.title} />
+              <h2>{blog.title}</h2>
+              <p>{blog.description}</p>
+              <Link to={`/blog/${blog.slug}`} className="read-more">
+                Read More
+              </Link>
+            </div>
+          ))}
+        </div>
+      </section>
+    </Layout>
   );
 }
