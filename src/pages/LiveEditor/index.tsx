19b169117d7070293c9cf904b9fcd2552de4dec2
Sivaprasath <121082414+sivaprasath2004@users.noreply.github.com>
2024-07-29 00:27:50 +0530

    remodify

---
+++ src/pages/LiveEditor/index.tsx
@@ -0,0 +1,67 @@
+import React,{useState} from "react";
+import Layout from "@theme/Layout";
+import  "./index.css";
+import BasicEditor from "./BasicEditor";
+const html="/icons/html-5.svg"
+const css="/icons/css.svg"
+const Js="/icons/js.svg"
+const react="/icons/jsx.svg"
+export default function LiveEditor() {
+  const [state,setState]=useState("none")
+  return (
+    <Layout>
+      <div
+        style={{
+          margin: "1rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        {state==="none"?
+        <div className="editor_select_container">
+          <div className="editor_container" onClick={()=>setState("basic")}>
+            <h2 className="editor_selcet_text">HTML CSS JS Editor</h2>
+            <div className="editor_select_imgage_container">
+            <img src={html} alt="html" className="editor_select_image" />
+              <h2>HTML</h2>
+            </div>
+            <div className="editor_select_imgage_container">
+              <img src={css} alt="css" className="editor_select_image" />
+              <h2>CSS</h2>
+            </div>
+            <div className="editor_select_imgage_container">
+            <img src={Js} alt="js" className="editor_select_image" />
+            <h2>JavaScript</h2>
+            </div>
+          </div>
+          <div className="editor_container" onClick={()=>setState("advance")}>
+          <h2 className="editor_selcet_text">React.Js Editor</h2>
+          <div className="editor_select_imgage_container">
+          <img src={react} alt="react" className="editor_select_image"/>
+          <h2>React.JS</h2>
+          </div>
+            </div>
+          </div>
+          :state==="advance"?
+         <iframe
+          src="https://codesandbox.io/embed/github/Ajay-Dhangar/my-react-app/main?fontsize=14&hidenavigation=1&theme=dark"
+          style={{
+            width: "100%",
+            height: "600px",
+            border: "0",
+            borderRadius: 8,
+            overflow: "hidden",
+            position: "static",
+            zIndex: 0,
+          }}
+          className="shadow-2xl"
+          title="dazzling-swanson-wne32"
+          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+          sandbox="allow-autoplay allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+        /> :<BasicEditor />
+}
+      </div>
+    </Layout>
+  );
+}
