dafa47ff5e457004a68c6610fd55af0ffe4dd132
Restyled.io <commits@restyled.io>
2024-08-18 09:57:00 +0000

    Restyled by prettier

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,34 +1,29 @@
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
-import AdsComponent from '@site/src/components/AdsComponent';
-import Author from '@site/src/components/Author';
-import BrowserWindow from '@site/src/components/BrowserWindow';
-import CollectionList from '@site/src/components/CollectionList';
-import Contributors from '@site/src/components/Contributors';
-import Courses from '@site/src/components/Courses';
-import ArrayVisualizations from
-    '@site/src/components/DSA/arrays/ArrayVisualizations';
-import BubbleSortVisualization from
-    '@site/src/components/DSA/arrays/BubbleSortVisualization';
-import InsertionSortVisualization from
-    '@site/src/components/DSA/arrays/InsertionSortVisualization';
-import SelectionSortVisualization from
-    '@site/src/components/DSA/arrays/SelectionSortVisualization';
-import File from '@site/src/components/File';
-import GiscusComponent from '@site/src/components/GiscusComponent';
-import Highlight from '@site/src/components/Highlight';
-import Lesson from '@site/src/components/Lesson';
-import SolutionAuthor from '@site/src/components/SolutionAuthor';
-import Table from '@site/src/components/Table';
-import TutorialAuthors from '@site/src/components/TutorialAuthors';
-import LinearSearchVisualizer from
-    '@site/src/dsa/searching-algorithms/LinearSearchVisualizer';
-import MDXComponents from '@theme-original/MDXComponents';
-import DocCardList from '@theme/DocCardList';
-import Image from '@theme/IdealImage';
-import TabItem from '@theme/TabItem';
-import Tabs from '@theme/Tabs';
-import {FaReact} from 'react-icons/fa';
+import AdsComponent from "@site/src/components/AdsComponent";
+import Author from "@site/src/components/Author";
+import BrowserWindow from "@site/src/components/BrowserWindow";
+import CollectionList from "@site/src/components/CollectionList";
+import Contributors from "@site/src/components/Contributors";
+import Courses from "@site/src/components/Courses";
+import ArrayVisualizations from "@site/src/components/DSA/arrays/ArrayVisualizations";
+import BubbleSortVisualization from "@site/src/components/DSA/arrays/BubbleSortVisualization";
+import InsertionSortVisualization from "@site/src/components/DSA/arrays/InsertionSortVisualization";
+import SelectionSortVisualization from "@site/src/components/DSA/arrays/SelectionSortVisualization";
+import File from "@site/src/components/File";
+import GiscusComponent from "@site/src/components/GiscusComponent";
+import Highlight from "@site/src/components/Highlight";
+import Lesson from "@site/src/components/Lesson";
+import SolutionAuthor from "@site/src/components/SolutionAuthor";
+import Table from "@site/src/components/Table";
+import TutorialAuthors from "@site/src/components/TutorialAuthors";
+import LinearSearchVisualizer from "@site/src/dsa/searching-algorithms/LinearSearchVisualizer";
+import MDXComponents from "@theme-original/MDXComponents";
+import DocCardList from "@theme/DocCardList";
+import Image from "@theme/IdealImage";
+import TabItem from "@theme/TabItem";
+import Tabs from "@theme/Tabs";
+import { FaReact } from "react-icons/fa";
 import LiteYouTubeEmbed from "react-lite-youtube-embed";
 
 export default {
@@ -58,5 +53,5 @@ export default {
   LiteYouTubeEmbed,
   Author,
   LinearSearchVisualizer,
-  AdsComponent
+  AdsComponent,
 };
