b473547be8a2af1e9b894cddb528b96e8b160ba5
Ajay Dhangar <99037494+ajay-dhangar@users.noreply.github.com>
2024-09-23 08:28:46 +0530

    Update index.tsx for remove 'useState'
    'useState' is defined but never used

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import React, { useState } from "react";
+import React from "react";
 import style from "./index.module.css";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
